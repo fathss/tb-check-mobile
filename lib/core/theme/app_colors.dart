829274febeb44c7c1d7536b6cb0b89620b3c7a5c
fathss <alwifathu@gmail.com>
2026-05-05 23:50:11 +0700

    feat: tambah custom font dan template warna

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color.fromRGBO(17, 98, 238, 1);
+  static const secondary = Color.fromRGBO(240, 245, 255, 1);
+
+  static const textPrimary = Colors.black;
+  static const textSecondary = Color.fromRGBO(163, 174, 194, 1);
+
+  static const success = Color.fromRGBO(102, 204, 107, 1);
+  static const warning = Color.fromRGBO(250, 159, 56, 1);
+  static const error = Color.fromRGBO(221, 60, 60, 1);
+}
