fc4901b19fb450945216cd459873f0495b793866
fathss <alwifathu@gmail.com>
2026-05-07 10:06:21 +0700

    feat: add complete profile page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tbcheck_app/core/theme/app_colors.dart';
 import 'package:tbcheck_app/features/admin_faskes/dashboard/screens/admin_dashboard_screen.dart';
 import 'package:tbcheck_app/features/landing/landing_page.dart';
+import 'package:tbcheck_app/features/user_profile/pages/complete_profile_page.dart';
 import 'features/admin_faskes/navigation/admin_main_navigation.dart';
 import 'package:tbcheck_app/core/navigation/main_page.dart';
 import 'package:tbcheck_app/features/home/home_page.dart';
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         fontFamily: "PlusJakartaSans",
         colorScheme: .fromSeed(seedColor: AppColors.primary),
       ),
-      home: const MainPage(),
+      home: const CompleteProfilePage(),
     );
   }
 }
